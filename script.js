800e1518a8ba26890717ff6177dc9738cd9db51f
trip6love <lanebyers13@gmail.com>
2022-07-25 03:19:13 -0400

    adding shade and randomness

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let drawing = false;
-ctx.lineWidth = 0.01;
+
+ctx.fillStyle = '#FFF5DE';
+
+ctx.shadowOffsetX = 0;
+ctx.shadowOffsetY = 10;
+ctx.shadowBlur = 10;
+ctx.shadowColor = 'rgba(0,0,0,0.5)';
+ctx.globalCompositeOperation = 'destination-over';
 
 class Root {
     constructor(x,y) {
@@ -14,29 +21,53 @@ class Root {
         this.y = y;
         this.speedX = Math.random() * 4 - 2;
         this.speedY = Math.random() * 4 - 2;
-        this.maxSize = Math.random() * 7 + 5;
-        this.size = Math.random() * 1 + 2;
-        this.vs = Math.random() * 0.2 + 0.05;
+        this.maxSize = Math.random() * 7 + 20;
+        this.size = 0;
+        this.vs = Math.random() * 0.2 + 0.5;
         this.angleX = Math.random() * 6.2;
         this.vax = Math.random() * 0.06 - 0.03;
         this.angleY = Math.random() * 6.2;
         this.vay = Math.random() * 0.06 - 0.03;
+        this.angle = 0;
+        this.va = Math.random() * 0.02 + 0.05;
         this.lightness = 10;
     }
     update(){
         this.x += this.speedX + Math.sin(this.angleX);
         this.y += this.speedY + Math.sin(this.angleY);
-        this.size += this.vs;
+        this.size += this.vs; 
         this.angleX += this.vax;
         this.angleY += this.vay;
+        this.angle += this.va;
         if(this.lightness < 70) this.lightness += 0.5;
         if(this.size < this.maxSize){
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-            ctx.fillStyle = 'hsl(130,100%,' + this.lightness + '%)';
-            ctx.fill();
-            ctx.stroke();
+
+            ctx.save();
+            ctx.translate(this.x, this.y);
+            ctx.rotate(this.angle);
+            
+            ctx.fillRect(0 - this.size/2, 0 - this.size/2, this.size, this.size);
+            let double = this.size * 2;
+            ctx.lineWidth = 0.1;
+            //ctx.strokeStyle = 'white';
+            //ctx.strokeRect(0 - double/2, 0 - double/2 , double, double);
+            //let triple = this.size * 3;
+            //ctx.lineWidth = 0.4;
+            //ctx.strokeStyle = 'green';
+           // ctx.strokeRect(0 - triple/2, 0 - triple/2 , triple, triple);
             requestAnimationFrame(this.update.bind(this));
+            ctx.restore();
+
+
+
+
+
+            //ctx.beginPath();
+            //ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+           // ctx.fillStyle = 'hsl(130,100%,' + this.lightness + '%)';
+            //ctx.fill();
+           // ctx.stroke();
+            
         }
     }
 }
